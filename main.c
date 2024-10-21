ee361f999dacfce4b3c317096121e413270f30c1
Tahir1072a <tahirifdn@gmail.com>
2024-10-21 20:49:49 +0300

    Word production added

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> //  strlen fonskiyonu kullanıldı.
+#include <time.h> //    Rastgele sayı üretmek için kullanıldı
+#include <math.h> //    sadece pow fonskiyonu kullanıldı
 
 #define Split 256
 #define Match 257
@@ -48,36 +51,58 @@ State* nfaBuilder(char* postfix);
 // Word Controls
 int checkWord(State* start, char* word);
 int isMatch(List *l);
-void addState(List *l, State *s);
-List* startList(State *s, List* l);
+void addState(List *l, State *s, int* listid);
+List* startList(State *s, List* l, int* listid);
 void Step(List* clist, int c, List* nlist);
 
 //Convert infix to postfix
 char* convertToPostfix(char* infix);
 
+// Word production
+void wordProduction(char* alphabet, int numberOfWords, State* start);
+char* separateAlphabet(char* alphabet);
+
 int main() {
-    char p[1000];
-    int control = 1;
+    srand(time(NULL));
+    char p[1000], alphabet[40];
+    int control = 1, numberOfWords = 0;
 
     l1.states = malloc(sizeof(State*));
     l2.states = malloc(sizeof(State*));
 
-    printf("Lütfen işlenecek infiz ifadeyi giriniz: \n");
+    printf("Lütfen üzerinde çalışılacak alfabeyi giriniz: Not: alfabeyi a,b,c,d şeklinde girin.\n");
+    scanf("%s", alphabet);
+    printf("Lütfen işlenecek infix ifadeyi giriniz: \n");
     scanf("%s", p);
     char* postfix = convertToPostfix(p);
     printf("İşlenecek olan postfix ifade: %s\n", postfix);
-
     State *start = nfaBuilder(postfix);
 
-    while (control) {
-        printf("Lütfen kontrol etmek istediğiniz kelimeyi giriniz:\n");
-        scanf("%s",p);
-        int result = checkWord(start, p);
+    printf("Ne işlemi yapmak istiyorsunuz?\n 1-Verdiğim kurala göre belli sayıda kelime üretmek istiyorum\n 2-Verdiğim kelimenin bu dile ait olup olmadığını öğrenmek istiyorum.\n");
+    printf("Lütfen 1 yada 2 numaralı tuşa basınız...\n");
+    scanf("%d", &control);
 
-        if (result == 1) printf("Evet bu kelime ilgili dile ait bir kelimedir: %s\n",p);
-        else printf("Bu kelime ilgili dile ait değildir! %s\n",p);
-        printf("Çıkmak için 0'e basiniz:\n");
-        scanf("%d", &control);
+    switch (control) {
+        case 1:
+            printf("Dile ait kaç kelime görmek istiyorsunuz?");
+            scanf("%d", &numberOfWords);
+            wordProduction(alphabet, numberOfWords, start);
+            break;
+        case 2:
+            while (control) {
+                printf("Lütfen kontrol etmek istediğiniz kelimeyi giriniz:\n");
+                scanf("%s",p);
+                int result = checkWord(start, p);
+
+                if (result == 1) printf("Evet bu kelime ilgili dile ait bir kelimedir: %s\n",p);
+                else printf("Bu kelime ilgili dile ait değildir! %s\n",p);
+                printf("Çıkmak için 0'e basiniz:\n");
+                scanf("%d", &control);
+            }
+            break;
+        default:
+            printf("Yanlış bir tuşlama yaptınız. Doğru tuşlama yapmayı öğrenene kadar lütfen programı kullanmayın!");
+            break;
     }
 
     return 0;
@@ -174,14 +199,14 @@ State* nfaBuilder(char* postfix) {
     // olmadığını gösterir. Bunun anlamı ise bu oklar final state bağlanması gerekir.
     e1 = Pop();
     patch(e1.outs, &matchState); //Okları önceden tanımlanmış matchState'e bağlar.
-    return e1.start;
+      return e1.start;
 }
 
 int checkWord(State* start, char* word) {
     if (word == NULL) return 0;
 
     List *clist, *nlist, *t;
-    clist = startList(start, &l1);
+    clist = startList(start, &l1, &listid);
     nlist = &l2;
     for(; *word; word++) {
         Step(clist, *word, nlist);
@@ -198,33 +223,32 @@ int isMatch(List *l) {
     return 0;
 }
 
-void addState(List *l, State *s) {
-    if(s == NULL || s->lastlist == listid) return;
-    s->lastlist = listid;
+void addState(List *l, State *s, int* listid) {
+    if(s == NULL || s->lastlist == *listid) return;
+    s->lastlist = *listid;
     if(s->c == Split) {
-        addState(l, s->out);
-        addState(l, s->out1);
+        addState(l, s->out,listid);
+        addState(l, s->out1, listid);
         return;
     }
     l->states[l->count++] = s;
 }
 
-List* startList(State *s, List* l) {
-    listid++;
+List* startList(State *s, List* l, int* listid) {
+    (*listid)++;
     l->count = 0;
-    addState(l,s);
+    addState(l,s, listid);
     return l;
 }
 
 void Step(List* clist, int c, List* nlist) {
     State *s;
-
     listid++;
     nlist->count = 0;
     for(int i = 0; i < clist->count; i++) {
         s = clist->states[i];
         if(s->c == c)
-            addState(nlist, s->out);
+            addState(nlist, s->out, &listid);
     }
 }
 
@@ -286,4 +310,68 @@ char* convertToPostfix(char* infix) {
     }
     *ptr = '\0';
     return postfix;
+}
+
+char* separateAlphabet(char* alphabet) {
+    char* setOfAlphabet = malloc(40 * sizeof(char));
+
+    for (int i = 0; strlen(alphabet) > i; i++) {
+        if (alphabet[i] == ',') continue;
+        char temp[2];
+        temp[0] = alphabet[i];
+        temp[1] = '\0';
+        strcat(setOfAlphabet, temp);
+    }
+
+    return setOfAlphabet;
+}
+
+int isWordInArray(char** words, int count, const char* word) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(words[i], word) == 0) {
+            return 1; // Kelime bulundu
+        }
+    }
+    return 0; // Kelime bulunamadı
+}
+
+void wordProduction(char* alphabet, int numberOfWords, State* start) {
+    char* setOfAlphabet = separateAlphabet(alphabet);
+    char  word[100] = "";
+    char** words = (char**)malloc((numberOfWords + 1) * sizeof(char*));
+    char** wordsp = words;
+
+    int generatedCount = 0;
+
+    for (int i = 1; numberOfWords > 0; i++) {
+        int calculated = (int)pow((int)strlen(setOfAlphabet), i) * i;
+        for (int j = 0; calculated > j; j++) {
+            if (numberOfWords <= 0) break;
+
+            char temp[2];
+            temp[0] = setOfAlphabet[rand() % (int)strlen(setOfAlphabet)];
+            temp[1] = '\0';
+
+            strcat(word, temp);
+
+            if (i == strlen(word)) {
+                if (checkWord(start, word) && !isWordInArray(words, generatedCount, word)) {
+                    for (int k = 0; k < strlen(words);k++) {
+
+                    }
+                    printf("%s\n", word);
+                    numberOfWords--;
+
+                    *wordsp = (char*)malloc((strlen(word) + 1) * sizeof(char));
+                    strcpy(*wordsp, word);
+                    wordsp++;
+
+                    generatedCount++;
+                }
+                word[0] = '\0';
+            }
+        }
+    }
+
+    free(words);
 }
